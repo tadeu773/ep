6d7e737674206fead1b3e783bf7d7d817914995c
tadeu773 <tadeuhbm@al.insper.edu.br>
2026-01-30 18:54:53 -0300

    funcao2

--- funcoes.py
+++ funcoes.py
@@ -9,3 +9,15 @@ def cria_pecas():
 
     random.shuffle(pecas)
     return pecas
+def inicia_jogo(n_jogadores, pecas):
+    jogadores = {}
+    for i in range(n_jogadores):
+        inicio = i * 7
+        fim = inicio + 7
+        jogadores[i] = pecas[inicio:fim]
+    monte = pecas[n_jogadores * 7:]
+    return {
+        'jogadores': jogadores,
+        'monte': monte,
+        'mesa': []
+    }
